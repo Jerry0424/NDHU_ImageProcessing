6b1eb8ddc237dfa9f089da5ab83dd8c2357ff7a4
0424jerry <410821319@gms.ndhu.edu.tw>
2021-12-28 01:13:38 +0800

    should be final version

--- Final_project/main.py
+++ Final_project/main.py
@@ -15,6 +15,7 @@ def read_image(img_arr, name_arr,path):
         name = Path(filename).stem
         name_arr.append(name)
 
+# do the cosin similarity
 def cosin_Sim(image, img, index):
     res = []
 
@@ -45,12 +46,12 @@ not_indata_img = []
 not_indata_img_name = []
 read_image(not_indata_img, not_indata_img_name, path)
 
-
+# in database check
 index = random.randint(0, 4)
-
 print("Chosen image in database is " + " : " + str(indata_img_name[index]))
+# do cosin similarity comparison between train images and one of images in indatabase file
 x = cosin_Sim(image, indata_img, index)
-
+# show the comparsion result and do the prediction
 print("================================================================")
 print("Comparison Result")
 print("================================================================")
@@ -61,3 +62,24 @@ print("End Comparison")
 print("================================================================")
 min_index = x.index(min(x))
 print("The person in chosen image is : " + str(image_name[min_index]))
+
+print("----------------------------------------------------------------")
+
+
+# not in database check
+index = random.randint(0, 4)
+
+print("Chosen image not in database is " + " : " + str(not_indata_img_name[index]))
+# do cosin similarity comparison between train images and one of images in not in database file
+y = cosin_Sim(image, not_indata_img, index)
+# show the comparsion result and do the prediction
+print("================================================================")
+print("Comparison Result")
+print("================================================================")
+for i in range(len(y)):
+    print(image_name[i] + " : " + str(y[i]))
+print("================================================================")
+print("End Comparison")
+print("================================================================")
+min_index = y.index(min(y))
+print("The person in chosen image most likely be: " + str(image_name[min_index]))
